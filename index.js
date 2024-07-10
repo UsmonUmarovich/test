b1000f632832daad870b434d9bd51735af39c461
Usmon <usmonW@icloud.com>
2024-07-10 16:57:09 +0500

    fixed bugs

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-app.listen(7070, () => {
-  console.log("Server is running on port 7070");
+app.listen(6000, () => {
+  console.log("Server is running on port 6000");
 });
