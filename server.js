15e5d664a641e96de869b266f8e6d80af7c11717
Usmon <usmonW@icloud.com>
2024-07-10 17:08:33 +0500

    fixningnigngig

--- server.js
+++ server.js
@@ -11,5 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(7474, () => {
-  console.log("Server is running on port 7474");
+  try {
+    console.log("Server is running on port 7474");
+  } catch (e) {
+    console.error("Server error", e);
+  }
 });
